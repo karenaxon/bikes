415d03c17286f98a0bde9a1a5ce4ec3d526d34ef
Kaxon <karenaxon@users.noreply.github.com>
2021-11-17 16:58:26 -0800

    Add updated index.js.
    Co-authored-by: Ella Tanttu <ellajtanttu@gmail.com>
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,16 +6,15 @@ import BikeInterface from './js/bikeInterface.js';
 import ManufacturerService from './js/manufacturer-service.js';
 
 let percentManuList;
-let manus = [];
+let manus;
 let location;
 function getElements(response) {
   if (response.bikes) {
-    $('#output').append(`<h3 class="resultsHead">The stolen bikes in ${location}: </h3>`);
-
+    manus = [];
+    $(".resultsHead").text(`The stolen bikes in ${location}:`);
     response.bikes.forEach(function(bike){
       manus.push(bike.manufacturer_name);
     });
-
     let index =0;
     response.bikes.forEach(function(bike) {
       $('#output').append(`<div class="result" id="${index}"></div>`);
@@ -31,24 +30,34 @@ function getElements(response) {
 }
 
 async function makeApiCall(location) {
-  const response = await BikeInterface.getBikes(location);
+  let response = await BikeInterface.getBikes(location);
   getElements(response);
 }
 
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
+    $("#output").empty();
+    $("#stats").hide();
+    $(".statsHead").hide();
+  
     location = $('#location').val();
+    $("#location").val("");
     makeApiCall(location);
   });
+  
   $('#findStatistics').on("click", function(){
+    $("#stats").empty();
+    $("#stats").show();
+    $(".statsHead").hide();
     if(manus){
+      percentManuList={};
       percentManuList=ManufacturerService.findPercentage(manus);
+      $(".statsHead").text("The stats on manufacturers: ");
+      $(".statsHead").show();
       Object.keys(percentManuList).forEach(function(key){
-        // let a = typeof percentManuList[key];
-        // alert(a);
-        let string= key+": "+((percentManuList[key])*10)+"%";
-        $("#stats").append(`<p>${string}</p>`);
+      let string= key+": "+((percentManuList[key])*10)+"%";
+      $("#stats").append(`<p>${string}</p>`);
       });
     }
   });
